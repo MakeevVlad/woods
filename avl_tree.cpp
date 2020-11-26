b977f7428d596a658e7be63f536f145c30b0ff90
MakeevVlad <vlad.mackeev2001@yandex.ru>
2020-11-26 12:06:41 +0300

    Building the shell

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -1,1 +1,48 @@
 #include "avl_tree.hpp"
+
+//=====Constructors for AVLtree ==================================
+template<typename T, class Node_>
+AVLtree<T, Node_>::AVLtree()
+{
+    T zero = 0;
+    root = new Node_(zero);
+}
+
+template<typename T, class Node_>
+AVLtree<T, Node_>::AVLtree()
+{
+    root = new Node_(value_);
+}
+
+//====Functions for AVLtree======================================
+template<typename T, class Node_>
+Node_* AVLtree<T, Node_>::insert(T value_)
+{
+    return root.insert(value_);
+}
+template<typename T, class Node_>
+Node_* AVLtree<T, Node_>::find(T value_)
+{
+    return root.find(value_);
+}
+
+template<typename T, class Node_>
+bool AVLtree<T, Node_>::erase(T value_)
+{
+    return root.erase(value_);
+}
+
+
+//====Constructors for Node=========================================
+template<typename T>
+Node<T>::Node(T value_)
+{
+    value = value_;
+    left = nullptr;
+    right = nullptr;
+    height = 1;
+}
+
+//====Functions for Node=========================================
+
+
--- avl_tree.hpp
+++ avl_tree.hpp
@@ -1,2 +1,68 @@
 #include <iostream>
 #include<vector>
+
+template<typename T, class Node_>
+class AbstractTree
+{
+public:
+    virtual Node_* find(T value_)
+    {
+        return nullptr;
+    }
+
+    virtual Node_* insert(T value_)
+    {
+        return nullptr;
+    }
+    
+    virtual bool erase(T value_)
+    {
+        return 0;
+    }
+};
+
+
+template<typename T>
+class Node
+{
+private:
+    T value;
+    size_t height;
+    Node* left;
+    Node* right;
+public:
+    Node(T value_);
+
+    Node* insert(T value_);
+
+    bool erase(T value_);
+
+    ~Node(){};
+};
+
+template<typename T, class Node_>
+class AVLtree : public AbstractTree<T, Node_>
+{
+private:
+    Node_* root;
+
+public:
+    AVLtree();
+
+    AVLtree(T value_);
+
+    AVLtree(std::vector<T> values_)
+    {
+        for(auto value_ : values_)
+            root = new Node_(value_);
+    }
+
+    Node_* insert(T value);
+
+    Node_* find(T value);
+
+    bool erase(T value);
+
+    ~AVLtree(){};
+
+};
